eaca0943ce65ee451de744f0ef38ead50cb41403
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-11-26 11:53:50 +0000

    Revert closing request body

--- handlers/backend_handler.go
+++ handlers/backend_handler.go
@@ -74,12 +74,6 @@ func newBackendTransport(connectTimeout, headerTimeout time.Duration, logger log
 	return
 }
 
-func closeBody(resp *http.Response) {
-	if resp != nil && resp.Body != nil {
-		resp.Body.Close()
-	}
-}
-
 func (bt *backendTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
 	resp, err = bt.wrapped.RoundTrip(req)
 	if err == nil {
@@ -89,7 +83,6 @@ func (bt *backendTransport) RoundTrip(req *http.Request) (resp *http.Response, e
 		logDetails := map[string]interface{}{"error": err.Error(), "status": 500}
 		defer bt.logger.LogFromBackendRequest(logDetails, req)
 		defer logger.NotifySentry(logger.ReportableError{ Error: err, Request: req, Response: resp })
-		defer closeBody(resp)
 
 		// Intercept some specific errors and generate an appropriate HTTP error response
 		if netErr, ok := err.(net.Error); ok {
